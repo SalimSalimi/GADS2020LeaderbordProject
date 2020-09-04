89c7f0f4f4692082e685d237eee4f7a5fcf12da5
SalimSalimi <salim.salimi@protonmail.com>
2020-09-04 18:21:34 +0200

    Showing data with the RecyclerView

--- app/src/main/java/dz/salim/salimi/gadsleaderbordproject/MainActivity.kt
+++ app/src/main/java/dz/salim/salimi/gadsleaderbordproject/MainActivity.kt
@@ -2,10 +2,20 @@ package dz.salim.salimi.gadsleaderbordproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import dz.salim.salimi.gadsleaderbordproject.ui.LeadersFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var leadersFragment: LeadersFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        leadersFragment = LeadersFragment()
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.main_frame, leadersFragment, "Fragment").commit()
     }
 }
--- app/src/main/java/dz/salim/salimi/gadsleaderbordproject/ui/LeadersFragment.kt
+++ app/src/main/java/dz/salim/salimi/gadsleaderbordproject/ui/LeadersFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dz.salim.salimi.gadsleaderbordproject.R
+import dz.salim.salimi.gadsleaderbordproject.data.models.Leader
+import dz.salim.salimi.gadsleaderbordproject.data.models.Learner
 
 class LeadersFragment : Fragment() {
 
     private lateinit var leadersRecyclerView: RecyclerView
+    private var linearLayoutManager: RecyclerView.LayoutManager? = null
+    private var leaderAdapter: LeadersAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,18 @@ class LeadersFragment : Fragment() {
         val rootView: View = inflater.inflate(R.layout.fragment_leaders, container, false)
 
         leadersRecyclerView = rootView.findViewById<RecyclerView>(R.id.leaders_list)
+        linearLayoutManager = LinearLayoutManager(context)
+        val listLeaders: ArrayList<Leader> = ArrayList()
+        listLeaders.add(Learner("Salim", 5, "Algeria", "URL"))
+        listLeaders.add(Learner("Salim", 5, "Algeria", "URL"))
+        listLeaders.add(Learner("Salim", 5, "Algeria", "URL"))
+
+        leaderAdapter = LeadersAdapter(listLeaders)
+
+        leadersRecyclerView.apply {
+            layoutManager = linearLayoutManager
+            adapter = leaderAdapter
+        }
 
         return rootView
     }
