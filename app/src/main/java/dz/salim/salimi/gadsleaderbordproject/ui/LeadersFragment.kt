ea37b51deb3c17d223b3deddde440ea3a83ebd3c
SalimSalimi <salim.salimi@protonmail.com>
2020-09-05 16:39:43 +0200

    Polish code

--- app/src/main/java/dz/salim/salimi/gadsleaderbordproject/ui/LeadersFragment.kt
+++ app/src/main/java/dz/salim/salimi/gadsleaderbordproject/ui/LeadersFragment.kt
@@ -1,7 +1,6 @@
 package dz.salim.salimi.gadsleaderbordproject.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,12 +10,10 @@ import androidx.recyclerview.widget.RecyclerView
 import dz.salim.salimi.gadsleaderbordproject.R
 import dz.salim.salimi.gadsleaderbordproject.data.DataRepository
 import dz.salim.salimi.gadsleaderbordproject.data.models.Leader
-import dz.salim.salimi.gadsleaderbordproject.data.models.Learner
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.Response
 
 class LeadersFragment : Fragment() {
 
@@ -24,48 +21,31 @@ class LeadersFragment : Fragment() {
     private var linearLayoutManager: RecyclerView.LayoutManager? = null
     private var leaderAdapter: LeadersAdapter? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         val rootView: View = inflater.inflate(R.layout.fragment_leaders, container, false)
-
-        leadersRecyclerView = rootView.findViewById<RecyclerView>(R.id.leaders_list)
-
-        //leaderAdapter = LeadersAdapter(listLeaders)
+        leadersRecyclerView = rootView.findViewById(R.id.leaders_list)
 
         return rootView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        getHoursLeaders()
         getSkillIqLeaders()
 
         super.onViewCreated(view, savedInstanceState)
-    }
-
-    private fun setupUi(adapter: LeadersAdapter) {
-        linearLayoutManager = LinearLayoutManager(context)
-        leadersRecyclerView.apply {
-            this.layoutManager = linearLayoutManager
-            this.adapter = adapter
-        }
     }
 
     private fun getHoursLeaders() {
         CoroutineScope(Dispatchers.IO).launch {
             val response = DataRepository.getHoursLeaders()
             if (response.isSuccessful) {
-                response.let {
-                    val adapter: LeadersAdapter = LeadersAdapter(it.body()!!)
-                    withContext(Dispatchers.Main) {
-                        setupUi(adapter)
-                    }
+                withContext(Dispatchers.Main) {
+                    setupUi(response.body()!!)
                 }
             }
         }
@@ -75,13 +55,19 @@ class LeadersFragment : Fragment() {
         CoroutineScope(Dispatchers.IO).launch {
             val response = DataRepository.getSkillLeaders()
             if (response.isSuccessful) {
-                response.let {
-                    val adapter: LeadersAdapter = LeadersAdapter(it.body()!!)
-                    withContext(Dispatchers.Main) {
-                        setupUi(adapter)
-                    }
+                withContext(Dispatchers.Main) {
+                    setupUi(response.body()!!)
                 }
             }
+        }
+    }
+
+    private fun setupUi(listLeaders: List<Leader>) {
+        this.linearLayoutManager = LinearLayoutManager(context)
+        this.leaderAdapter = LeadersAdapter(listLeaders)
+        this.leadersRecyclerView.apply {
+            this.layoutManager = linearLayoutManager
+            this.adapter = leaderAdapter
         }
     }
 }
