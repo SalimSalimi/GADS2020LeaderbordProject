a33043a8998ba706f2b9ea7d7358d77ca26d918d
SalimSalimi <salim.salimi@protonmail.com>
2020-09-06 21:47:55 +0200

    Replacing the leader_item to a Cardview

--- app/src/main/java/dz/salim/salimi/gadsleaderbordproject/MainActivity.kt
+++ app/src/main/java/dz/salim/salimi/gadsleaderbordproject/MainActivity.kt
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentTransaction
 import dz.salim.salimi.gadsleaderbordproject.ui.FormView.FormFragment
+import dz.salim.salimi.gadsleaderbordproject.ui.LeadersView.LeadersTabFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val leadersFragment = FormFragment()
+        val leadersFragment = LeadersTabFragment()
         val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.add(R.id.main_frame, leadersFragment, "Fragment").commit()
     }
