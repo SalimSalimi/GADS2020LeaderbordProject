6e713991602da0803651ac9cbb55290f88bda006
SalimSalimi <salim.salimi@protonmail.com>
2020-09-06 15:13:45 +0200

    Implementing the form logic

--- app/src/main/java/dz/salim/salimi/gadsleaderbordproject/ui/FormView/FormFragment.kt
+++ app/src/main/java/dz/salim/salimi/gadsleaderbordproject/ui/FormView/FormFragment.kt
@@ -6,14 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dz.salim.salimi.gadsleaderbordproject.R
+import dz.salim.salimi.gadsleaderbordproject.data.DataRepository
+import dz.salim.salimi.gadsleaderbordproject.data.models.Form
+import kotlinx.android.synthetic.main.fragment_form.*
+import kotlinx.android.synthetic.main.leader_list_item.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class FormFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,4 +24,21 @@ class FormFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_form, container, false)
     }
 
+    private fun postForm() {
+        val email = email_textview.text.toString()
+        val name = name_text_view.text.toString()
+        val lastName = last_name_textview.text.toString()
+        val githubLink = github_textview.text.toString()
+
+        val form = Form(email, name, lastName, githubLink)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = DataRepository.postForm(form)
+            if (response.isSuccessful) {
+                //TODO Implementing the logic
+            } else {
+                //TODO Implementing the logic
+            }
+        }
+    }
 }
