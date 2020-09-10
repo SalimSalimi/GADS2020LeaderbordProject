229a73e83bd64b404240ef69875d03e5458c3547
SalimSalimi <salim.salimi@protonmail.com>
2020-09-11 00:20:44 +0200

    Implementing the background and styles

--- app/src/main/java/dz/salim/salimi/gadsleaderbordproject/MainActivity.kt
+++ app/src/main/java/dz/salim/salimi/gadsleaderbordproject/MainActivity.kt
@@ -3,8 +3,12 @@ package dz.salim.salimi.gadsleaderbordproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toolbar
 import androidx.fragment.app.FragmentTransaction
 import dz.salim.salimi.gadsleaderbordproject.ui.LeadersView.LeadersTabFragment
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,20 +16,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupLeaderFragment()
-
         submit_button_toolbar.setOnClickListener {
-            submitBtnClicked()
+            onSubmitToolbarBtnClicked()
         }
+        setupLeaderFragment()
     }
 
     private fun setupLeaderFragment() {
+        gads_imageview.visibility = View.GONE
         val leadersFragment = LeadersTabFragment()
         val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.add(R.id.main_frame, leadersFragment, "Fragment").commit()
+        toolbar.visibility = View.VISIBLE
     }
 
-    private fun submitBtnClicked(){
+    private fun onSubmitToolbarBtnClicked() {
         val intent = Intent(this, FormActivity::class.java)
         startActivity(intent)
     }
