c2da62e4ff5a0b573097e24dd4200b65e97bf10e
SalimSalimi <salim.salimi@protonmail.com>
2020-09-10 19:10:33 +0200

    Implementing custom dialog result

--- app/src/main/java/dz/salim/salimi/gadsleaderbordproject/ui/FormView/FormFragment.kt
+++ app/src/main/java/dz/salim/salimi/gadsleaderbordproject/ui/FormView/FormFragment.kt
@@ -9,6 +9,9 @@ import androidx.appcompat.app.AlertDialog
 import dz.salim.salimi.gadsleaderbordproject.R
 import dz.salim.salimi.gadsleaderbordproject.data.DataRepository
 import dz.salim.salimi.gadsleaderbordproject.data.models.Form
+import dz.salim.salimi.gadsleaderbordproject.utils.SubmitResult
+import kotlinx.android.synthetic.main.custom_confirm_dialog.*
+import kotlinx.android.synthetic.main.custom_result_confirm_dialog.*
 import kotlinx.android.synthetic.main.fragment_form.*
 import kotlinx.android.synthetic.main.leader_list_item.*
 import kotlinx.coroutines.CoroutineScope
@@ -43,25 +46,50 @@ class FormFragment : Fragment() {
         CoroutineScope(Dispatchers.IO).launch {
             val response = DataRepository.postForm(form)
             if (response.isSuccessful) {
-                //TODO Implementing the logic
+                resultDialog(SubmitResult.SUCCESS)
             } else {
-                //TODO Implementing the logic
+                resultDialog(SubmitResult.FAILED)
             }
         }
     }
 
     private fun confirmDialog() {
+        val confirmDialog: AlertDialog = buildDialog(R.layout.custom_confirm_dialog)
+        confirmDialog.show()
+        confirmDialog.confirm_custom_button.setOnClickListener {
+            onConfirmBtnClicked()
+        }
+    }
+
+    private fun resultDialog(result: SubmitResult) {
+        val resultDialog = buildDialog(R.layout.custom_result_confirm_dialog)
+        resultDialog.show()
+        when (result) {
+            SubmitResult.FAILED -> {
+                resultDialog.result_submit_textview.text = getString(R.string.submission_not_successful)
+                resultDialog.result_submit_imageview.setImageResource(R.drawable.ic_warning)
+            }
+            SubmitResult.SUCCESS -> {
+                resultDialog.result_submit_textview.text = getString(R.string.submission_successful)
+                resultDialog.result_submit_imageview.setImageResource(R.drawable.ic_confirm_circle)
+            }
+        }
+    }
+
+    private fun buildDialog(resource: Int): AlertDialog {
         val dialogBuilder = AlertDialog.Builder(context!!)
-        val dialogLayout: View = LayoutInflater.from(context).inflate(R.layout.custom_confirm_dialog, null)
+        val dialogLayout: View = LayoutInflater.from(context).inflate(resource, null)
 
         dialogBuilder.setView(dialogLayout)
 
-        val alertDialog = dialogBuilder.create()
-        alertDialog.show()
-
+        return dialogBuilder.create()
     }
 
     private fun onSubmitClickBtn() {
         confirmDialog()
+
+    }
+    private fun onConfirmBtnClicked() {
+        postForm()
     }
 }
---
+++ app/src/main/java/dz/salim/salimi/gadsleaderbordproject/utils/SubmitResult.kt
@@ -0,0 +1,5 @@
+package dz.salim.salimi.gadsleaderbordproject.utils
+
+enum class SubmitResult {
+    SUCCESS, FAILED
+}
