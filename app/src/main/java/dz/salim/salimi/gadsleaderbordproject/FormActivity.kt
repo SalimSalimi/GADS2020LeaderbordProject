8ad2a5bddaace7a817457d6e9094253ab9c8d284
SalimSalimi <salim.salimi@protonmail.com>
2020-09-11 13:11:11 +0200

    Adding back button and chaging gads background to transparent

--- app/src/main/java/dz/salim/salimi/gadsleaderbordproject/FormActivity.kt
+++ app/src/main/java/dz/salim/salimi/gadsleaderbordproject/FormActivity.kt
@@ -2,13 +2,18 @@ package dz.salim.salimi.gadsleaderbordproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.FragmentTransaction
 import dz.salim.salimi.gadsleaderbordproject.ui.FormView.FormFragment
 
 class FormActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+        setupToolbar()
         setupFormFragment()
     }
 
@@ -16,5 +21,20 @@ class FormActivity : AppCompatActivity() {
         val formFragment = FormFragment()
         val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.add(R.id.form_fragment, formFragment, "Form").commit()
+    }
+
+    private fun setupToolbar() {
+        toolbar = findViewById(R.id.form_toolbar)
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
